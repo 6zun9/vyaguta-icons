a0bd351044f2234803a0122b7bfdbe5325b3e1b4
Prabesh <Prabesh@prabesh.local>
2020-04-20 21:24:11 +0545

    Add dist files

---
+++ dist/lib/esm/iconsManifest.js
@@ -0,0 +1,13 @@
+export var IconsManifest = [{
+  "id": "fi",
+  "name": "Feather",
+  "projectUrl": "https://feathericons.com/",
+  "license": "MIT",
+  "licenseUrl": "https://github.com/feathericons/feather/blob/master/LICENSE"
+}, {
+  "id": "ui",
+  "name": "Unicons",
+  "projectUrl": "https://iconscout.com/unicons",
+  "license": "Apache-2.0",
+  "licenseUrl": "https://github.com/Iconscout/unicons/blob/master/LICENSE"
+}];
